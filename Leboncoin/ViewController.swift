3ace7877c99bf4192eaad492f7154508d0f1e8b4
Óscar R. Garrucho <oscar.garrucho@gmail.com>
2025-02-25 11:43:23 +0100

    Initial commit

---
+++ Leboncoin/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Leboncoin
+//
+//  Created by Oscar Rodriguez Garrucho on 25/2/25
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2025 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LeboncoinUITests/LeboncoinUITestsLaunchTests.swift
@@ -0,0 +1,34 @@
+//
+//  LeboncoinUITestsLaunchTests.swift
+//  Leboncoin
+//
+//  Created by Oscar Rodriguez Garrucho on 25/2/25
+//  Linkedin: https://www.linkedin.com/in/oscar-garrucho/
+//  Copyright © 2025 Oscar Rodriguez Garrucho. All rights reserved.
+//
+
+import XCTest
+
+final class LeboncoinUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
